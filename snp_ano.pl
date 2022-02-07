ddc326a2e86e5cf9d56a312d1de6f11cf9a88665
kalpanaiiita <99169062+kalpanaiiita@users.noreply.github.com>
2022-02-07 14:29:24 +0530

    Create snp_ano.pl

---
+++ snp_ano.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl-w
+use strict;
+use warnings;
+use autodie;
+die "usage: $0  " unless @ARGV==3;
+
+my $in=shift;
+my $in2=shift;
+my $ot=shift;
+
+open my $o, ">",$ot;
+open my $i, "<",$in;
+while (my $l =<$i>){
+        if ($l !~ /#/ ){
+                my @x=split /\t/,$l;
+                open my $i2,"<",$in2;
+                while (my $l2=<$i2>){
+                        my @d =split /\t/,$l2;
+                        if ($d[1] eq $x[0] && $d[2] == $x[1] ) {
+                                    print "$l$l2\n";
+                        }
+                }
+         }
+}
